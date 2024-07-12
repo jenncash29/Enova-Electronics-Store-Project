a07fc596868bbc9b64296bba2901ab90906a0042
Jennifer Cash <143119738+jenncash29@users.noreply.github.com>
2024-07-12 15:55:28 -0400

    Update SQL queries.sql

--- SQL queries.sql
+++ SQL queries.sql
@@ -1,21 +1,42 @@
---1. Calculating the order counts, sales, and AOV for Macbooks sold in North America for each quarter across all years. 
+--1. Calculating the order counts, revenue, and AOV for Macbooks sold in North America per each quarter across all years. 
 SELECT
   DATE_TRUNC(purchase_ts, quarter) AS quarter,
   COUNT(DISTINCT orders.id) AS order_count,
-  ROUND(SUM(usd_price), 2) AS total_sales_usd,
-  ROUND(AVG(usd_price), 2) AS aov_usd
+  ROUND(SUM(usd_price), 2) AS total_revenue,
+  ROUND(AVG(usd_price), 2) AS aov
 FROM core.orders 
 LEFT JOIN core.customers 
   ON orders.customer_id = customers.id
 LEFT JOIN core.geo_lookup 
   ON customers.country_code = geo_lookup.country
-WHERE product_name = "Macbook Air Laptop"
+WHERE product_name = 'Macbook Air Laptop'
   AND region = 'NA'
 GROUP BY 1
-ORDER by 1;
+ORDER BY 1;
 
 
---2. Identifying the region that has the average highest time to deliver for products purchased in 2022 on the website or products purchased on mobile in any year.  
+--2. Calculating the average quarterly order count and revenue for MacBooks sold in North America across all quarters.
+WITH quarterly_metrics AS (
+  SELECT  
+    DATE_TRUNC(purchase_ts, quarter) AS quarter,
+    COUNT(DISTINCT orders.id) AS order_count,
+    SUM(usd_price) AS total_revenue
+  FROM core.orders 
+  LEFT JOIN core.customers 
+    ON orders.customer_id = customers.id
+  LEFT JOIN core.geo_lookup 
+    ON customers.country_code = geo_lookup.country
+  WHERE product_name = 'Macbook Air Laptop'
+    AND region = 'NA'
+  GROUP BY 1)
+
+SELECT 
+  ROUND(AVG(order_count), 2) AS avg_quarter_orders,
+  ROUND(AVG(total_revenue), 2) AS avg_quarter_revenue
+FROM quarterly_metrics;
+
+
+--3. Identifying the region that has the average highest time to deliver for products purchased in 2022 on the website or products purchased on mobile in any year.  
 SELECT
   geo_lookup.region,
   AVG(DATE_DIFF(order_status.delivery_ts, order_status.purchase_ts, day)) AS delivery_days
@@ -34,7 +55,7 @@ ORDER BY 2 DESC
 LIMIT 1;
 
 
---3. Calculating the refund rate and refund count for each product overall.
+--4. Calculating the refund rate and refund count for each product overall.
 SELECT
   CASE WHEN product_name = '27in"" 4k gaming monitor' THEN '27in 4K gaming monitor' ELSE product_name END AS cleaned_product_name,
   SUM(CASE WHEN refund_ts IS NOT NULL THEN 1 ELSE 0 END) AS refund_count,
@@ -46,7 +67,7 @@ GROUP BY 1
 ORDER BY 3 DESC;
 
 
---4. Identifying the most popular product within each region.
+--5. Identifying the most popular product within each region.
 --This query can be done as a CTE or in one query using QUALIFY. 
 SELECT
   region,
@@ -62,14 +83,29 @@ QUALIFY ROW_NUMBER() OVER (PARTITION BY region ORDER BY COUNT(DISTINCT orders.id
 ORDER BY 3 DESC;
 
 
---5. Comparing the YoY differences in time taken to make a purchase between loyalty customers vs. non-loyalty customers.
+--6. Comparing the YoY differences in time taken to make a purchase between loyalty customers vs. non-loyalty customers.
 SELECT
-  EXTRACT(year FROM purchase_ts) as year,
+  EXTRACT(year FROM purchase_ts) AS year,
   CASE WHEN loyalty_program = 1 THEN 'YES' ELSE 'NO' END AS is_loyalty,
   ROUND(AVG(DATE_DIFF(purchase_ts, created_on, day)), 1) AS days_to_purchase,
   ROUND(AVG(DATE_DIFF(purchase_ts, created_on, month)), 1) AS months_to_purchase
 FROM core.orders 
 LEFT JOIN core.customers 
   ON orders.customer_id = customers.id
+GROUP BY 1, 2
+ORDER BY 1;
+
+
+--7. Identifying and ranking each country within its region based on total revenue using a window function.
+SELECT
+  region,
+  country,
+  ROUND(SUM(usd_price), 2) AS total_revenue,
+  RANK() OVER(PARTITION BY region ORDER BY SUM(usd_price) DESC) AS country_rank
+FROM core.orders
+LEFT JOIN core.customers
+  ON orders.customer_id = customers.id
+LEFT JOIN core.geo_lookup 
+  ON customers.country_code = geo_lookup.country
 GROUP BY 1, 2
 ORDER BY 1;
